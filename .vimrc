288d862b2de3d76474c3ea9736ebcf4827b51b10
Raghavan Chockalingam <rachocka@cisco.com>
2016-11-29 22:33:59 +0000

    subscriber-udp-notify: sending udp messages to subscriber for every 1s on motion detection status
    Change-Id: I94d174d4b0fe6a7eb4c427f26a4ac8dc8c4a7760

---
+++ .vimrc
@@ -0,0 +1,12 @@
+set tabstop=4 shiftwidth=4 expandtab
+set number
+syntax off
+set ignorecase
+nmap <CR> G
+
+set colorcolumn=140
+
+set nobackup
+set noswapfile
+set nowritebackup
+setlocal cm=blowfish
--- motionsensor.py
+++ motionsensor.py
@@ -1,25 +1,31 @@
 import RPi.GPIO as GPIO
 import time
+import socket
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-
-LED_PIN=3
-GPIO.setup(LED_PIN,GPIO.OUT)     #Define pin 3 as an output pin
 
 PIR_SENSOR_PIN=7
 GPIO.setup(PIR_SENSOR_PIN, GPIO.IN)
 
+# subscriber
+host=localhost
+port=5005
+
 TIME_TO_SLEEP=1
-
 while True:
-       i=GPIO.input(PIR_SENSOR_PIN)
-       time.sleep(TIME_TO_SLEEP)
-       if i==0:                 #When output from motion sensor is LOW
-             print "No intruders",i
-             GPIO.output(LED_PIN, 0)  #Turn OFF LED
-             #time.sleep(0.1)
-       elif i==1:               #When output from motion sensor is HIGH
-             print "Intruder detected",i
-             GPIO.output(LED_PIN, 1)  #Turn ON LED
-             
+  i = GPIO.input(PIR_SENSOR_PIN)
+  time.sleep(TIME_TO_SLEEP)
 
+  if i==0:   #When output from motion sensor is LOW
+    print "No intruders"
+    notify_subscriber("no motion detected")
+     
+   
+  elif i==1: #When output from motion sensor is HIGH
+    print "Intruder detected"
+    notify_subscriber("motion detected")
+          
+def notify_subscriber(p):
+  sock = socket.socket(socket.AF_NET, socket.SOCK_DGRAM)
+  sock.sendto(p, (host, port) )
