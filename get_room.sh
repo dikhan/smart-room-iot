2592a6428d84f839ccad99448db16578a8a65e58
Daniel Isaac Khan Ramiro <dkhanram@cisco.com>
2016-12-01 23:15:44 +0000

    Update script to parameterise username too
    - Fixed bug with exchange resource not being loaded properly

--- get_room.sh
+++ get_room.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-[[ -z "$1" ]] &&  echo "Input Error: requires password" && exit -1
+[[ -z "$1" ]] &&  echo "Input Error: requires user" && exit -1
+[[ -z "$2" ]] &&  echo "Input Error: requires password" && exit -1
 
-password=$1
+user=$1
+password=$2
 
-curl -s -H 'Content-Type: text/xml;charset=utf-8' --data @/tmp/check_room_status --ntlm -u rachocka:$password  https://mail.cisco.com/ews/exchange.asmx
+curl -s -H 'Content-Type: text/xml;charset=utf-8' --data @/tmp/check_room_status --ntlm -u $user:$password  https://mail.cisco.com/ews/exchange.asmx
